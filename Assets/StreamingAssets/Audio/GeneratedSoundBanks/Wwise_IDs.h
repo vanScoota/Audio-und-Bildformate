07de1085497e5250c704e92a3b681d1a83a4c71f
vanScoota <maxi.schoen@web.de>
2019-02-06 20:15:08 +0100

    Abgabe

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,64 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BIRDS = 352130103U;
+        static const AkUniqueID CAR = 983016381U;
+        static const AkUniqueID FOOTSTEP = 1866025847U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID LANDING = 2548270042U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GRASS = 4248645337U;
+                static const AkUniqueID MUDD = 3242545539U;
+                static const AkUniqueID NORMAL = 1160234136U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMBIENT = 77978275U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REVERB = 348963605U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
---
+++ README.md
@@ -0,0 +1,11 @@
+# SurroundSoundProject
+
+SurroundSoundProject ist ein Projekt von Niklas Amann und Maximilian Schön im Rahmen der Vorlesung *Audio- und Bildformate* an der Hochschule Aalen.
+Es handelt sich hierbei um ein Unity-Projekt, bei welchem das Hauptaugenmerk auf der Implementierung von Surround-Sounds liegt.
+Dafür wurde die Audio-Engine *Audiokintetic Wwise* verwendet.
+
+Folgende Cases wurden implementiert:
+- Je nach Untergrund wechselnde Schrittgeräusche.
+- Hallende Schritte in einem Raum.
+- Stationäres Vogelgezwitscher.
+- Mobiles Automotorengeräusch.
